76edd7e440ecb49bd3a864fd9c3e1949e5dc6577
Benjamin Evans <benjaminjosephevans1@gmail.com>
2017-07-27 13:38:29 +0100

    Create dependentoptionset.js

---
+++ dependentoptionset.js
@@ -0,0 +1,55 @@
+///<reference path="MSXRMTOOLS.Xrm.Page.2016.js"/>
+if (Slicedbread === undefined) {
+    var Slicedbread = {};
+    var source;
+    var secondarySource;
+    //   console.log("namespace generated");
+}
+Slicedbread.onLoad = function (parentOptionSet, childOptionSet) {
+    source = Xrm.Page.getAttribute(parentOptionSet);
+    secondarySource = Xrm.Page.getAttribute(childOptionSet);
+    // console.log("sources discovered");
+    if (source.getValue() === null) {
+        Xrm.Page.getControl(childOptionSet).setDisabled(true);
+    }
+    else {
+        Slicedbread.Init(parentOptionSet, childOptionSet);
+    }
+    //Xrm.Utility.alertDialog(childOptionSet);
+}
+Slicedbread.Init = function (parentOptionSet, childOptionSet) {
+    var secondarySourceControl = Xrm.Page.getControl(childOptionSet);
+    var secondarySourceOptions = secondarySourceControl.getOptions();
+    for (var i = 0; i < secondarySourceOptions.length; i++) {
+        if (secondarySourceOptions[i].value.toString().substring(3, 6) !== source.getValue().toString().substring(3, 6)) {
+            secondarySourceControl.removeOption(secondarySourceOptions[i].value);
+            //console.log("hello");
+        }
+    }
+}
+Slicedbread.DependentOptionSet = function (parentOptionSet, childOptionSet) {
+    var sourceValue = source.getValue();
+    var childOptionSetControl = Xrm.Page.getControl(childOptionSet);
+    if (sourceValue === "" || sourceValue === -1 || sourceValue == null) {
+        childOptionSetControl.clearOptions();
+        Xrm.Page.getControl(childOptionSet).setDisabled(true);
+        return;
+    }
+    if (Xrm.Page.getControl(childOptionSet).getDisabled()) {
+        Xrm.Page.getControl(childOptionSet).setDisabled(false);
+    }
+    var sourceValueString = sourceValue.toString();
+    // console.log(sourceValue);
+    var sourceValueSubstring = sourceValueString.substring(3, 6);
+    var secondarySourceOptions = secondarySource.getOptions();
+    childOptionSetControl.clearOptions();
+    for (var i = 0; i < secondarySourceOptions.length; i++) {
+        if (secondarySourceOptions[i].value !== null) {
+            var secondarySourceSubString = secondarySourceOptions[i].value.toString().substring(3, 6);
+            if (secondarySourceSubString === sourceValueSubstring) {
+                //console.log(secondarySourceOptions[i]);
+                childOptionSetControl.addOption(secondarySourceOptions[i], i);
+            }
+        }
+    }
+}
